f7b655d2c5fc918b5d619f2dfce6e726f1444957
PeasantL <samuel.tan99@rocketmail.com>
2024-05-22 19:12:42 +0800

    Changed to venv. Removed docker. Added run script.

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the activate script exists
+if [ -f "./.venv/bin/activate" ]; then
+    echo "Activating existing virtual environment."
+    source "./.venv/bin/activate"
+else
+    echo "Virtual environment does not exist. Creating virtual environment."
+    python3 -m venv .venv
+    source "./.venv/bin/activate"
+    pip install -r requirements.txt  
+fi
+
+# Run the inference script
+python3 server.py
